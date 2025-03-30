b292e55b2147f24cccddefd04cc530a5b5a7c9b7
Adrian Robles <adrianrobles2706@gmail.com>
2025-03-30 19:00:25 +0200

    Cambio en nombre de un archivo

--- src/components/MFAsetup.js
+++ src/components/MFAsetup.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./MFASetup.css";
+import "./MFAsetup.css";
 
 
 const MFASetup = ({ username, onMFASuccess }) => {
